3176a765679ee8daf7fb1d3532f8c8f9bce264f2
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-02-08 17:02:41 +0100

    feat: implement api client

---
+++ app/lib/apiClient.ts
@@ -0,0 +1,70 @@
+const baseURL = "http://127.0.0.1:8000/api/v1";
+
+interface FetchOptions {
+  method?: "GET" | "POST" | "PUT" | "DELETE";
+  params?: Record<string, string | number>;
+  body?: unknown;
+  headers?: HeadersInit;
+}
+
+async function apiFetch<T>(
+  endpoint: string,
+  { method = "GET", params = {}, body, headers = {} }: FetchOptions = {},
+): Promise<T> {
+  // initialize base url
+  const url = new URL(baseURL + endpoint);
+
+  // convert params object to array of key value pairs and append to the url
+  Object.entries(params).forEach(([key, value]) => {
+    url.searchParams.append(key, String(value));
+  });
+
+  try {
+    // set options and fetch data
+    const response = await fetch(url.toString(), {
+      method,
+      headers: {
+        "Content-Type": "application/json",
+        ...headers,
+      },
+      body: body ? JSON.stringify(body) : undefined,
+    });
+
+    return response.json();
+  } catch (error) {
+    throw new Error(`API error`);
+  }
+}
+
+export class ApiClient<T> {
+  endpoint: string;
+
+  constructor(endpoint: string) {
+    this.endpoint = endpoint;
+  }
+
+  getAll = (params?: Record<string, string | number>) => {
+    return apiFetch<T>(this.endpoint, { params });
+  };
+
+  get = (id: number | string) => {
+    return apiFetch<T>(`${this.endpoint}/${id}`);
+  };
+
+  post = <B>(body: B) => {
+    return apiFetch<T>(this.endpoint, { method: "POST", body });
+  };
+
+  update = <B>(id: number | string, body: B) => {
+    return apiFetch<T>(`${this.endpoint}/${id}`, {
+      method: "PUT",
+      body,
+    });
+  };
+
+  delete = (id: number | string) => {
+    return apiFetch<void>(`${this.endpoint}/${id}`, {
+      method: "DELETE",
+    });
+  };
+}
