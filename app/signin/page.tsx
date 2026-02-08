be397424b353776db37c556147a37efd65438974
Chiemerie Ejike Mbah <ejike162@gmail.com>
2026-02-08 11:25:05 +0100

    feat: add signin form

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,10 +1,12 @@
 import SignInHeader from "../ui/SigninPage/SignInHeader";
+import SignInForm from "../ui/SigninPage/SignInForm";
 import { VStack } from "@chakra-ui/react";
 
 const SigninPage = () => {
   return (
-    <VStack w={"490px"} h={"548.8px"} gap={"15px"} marginX={"auto"} bg={"red"}>
+    <VStack w={"490px"} h={"548.8px"} gap={"15px"} marginX={"auto"}>
       <SignInHeader />
+      <SignInForm />
     </VStack>
   );
 };
---
+++ app/ui/SigninPage/SignInForm.tsx
@@ -0,0 +1,128 @@
+"use client";
+
+import {
+  Box,
+  Field,
+  HStack,
+  Input,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
+import { useForm } from "react-hook-form";
+import { PasswordInput } from "../../../components/ui/password-input";
+import { GoogleIcon } from "./GoogleIcon";
+import LinkText from "./LinkText";
+import SignInButton from "./SignInButton";
+import SeparatorText from "./SeparatorText";
+
+interface FormValues {
+  username: string;
+  password: string;
+}
+
+const SignInForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>();
+
+  const onSubmit = handleSubmit((data) => console.log(data));
+
+  return (
+    <Box w={"full"}>
+      <form onSubmit={onSubmit}>
+        <Stack gap="4" align="flex-start">
+          <Field.Root
+            invalid={!!errors.username}
+            h={"84px"}
+            gap={"10px"}
+            w={"490px"}
+          >
+            <Field.Label
+              w={"170px"}
+              h={"24px"}
+              gap={"4px"}
+              fontWeight={"400px"}
+              lineHeight={"24px"}
+              letterSpacing={"-0.11px"}
+            >
+              Email Address/Username *
+            </Field.Label>
+            <Input
+              {...register("username")}
+              placeholder="Enter email address"
+              h={"50px"}
+              borderRadius={"7px"}
+              borderWidth={"2px"}
+              borderColor={"#292929"}
+            />
+            <Field.ErrorText>{errors.username?.message}</Field.ErrorText>
+          </Field.Root>
+
+          <Field.Root
+            invalid={!!errors.password}
+            h={"84px"}
+            gap={"10px"}
+            w={"490px"}
+          >
+            <Field.Label
+              w={"170px"}
+              h={"24px"}
+              gap={"4px"}
+              fontWeight={"400px"}
+              lineHeight={"24px"}
+              letterSpacing={"-0.11px"}
+            >
+              Password *
+            </Field.Label>
+            <PasswordInput
+              {...register("password")}
+              placeholder="Enter password"
+              h={"50px"}
+              borderRadius={"7px"}
+              borderWidth={"2px"}
+              borderColor={"#292929"}
+            />
+            <Field.ErrorText>{errors.password?.message}</Field.ErrorText>
+          </Field.Root>
+
+          <VStack h={"94px"} gap={"20px"}>
+            <LinkText textAlign="right">Forgot Password?</LinkText>
+            <SignInButton type="submit">Sign In</SignInButton>
+            <SeparatorText />
+            <SignInButton>
+              <GoogleIcon w={"23.24px"} h={"23.71px"} />
+              <Text
+                fontWeight={"600"}
+                fontSize={"16.17px"}
+                lineHeight={"23.71px"}
+                letterSpacing={"0px"}
+              >
+                Sign In With Google
+              </Text>
+            </SignInButton>
+
+            <HStack w={"288px"} h={"26px"} alignItems={"center"}>
+              <Text
+                fontWeight={"400"}
+                fontSize={"16px"}
+                lineHeight={"25.87px"}
+                letterSpacing={"-0.11px"}
+                color={"#8E9BAE"}
+                flexShrink="0"
+                marginLeft={"43.5px"}
+              >
+                Don’t have an account?
+              </Text>
+              <LinkText>Sign Up</LinkText>
+            </HStack>
+          </VStack>
+        </Stack>
+      </form>
+    </Box>
+  );
+};
+
+export default SignInForm;
